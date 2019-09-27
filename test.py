cb86e3b74fa4122dc4b4439e07ebaf74c303b8c6
unknown <1960595754@qq.com>
2019-09-27 09:34:22 +0800

    commit

--- test.py
+++ test.py
@@ -18,6 +18,7 @@ from lxml import etree
 from pathlib import Path
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from collections import Counter
 from fake_useragent import UserAgent
 from decimal import Decimal, ROUND_HALF_UP
 
@@ -30,6 +31,9 @@ def path():
 
 
 async def test_io():
+    """
+    异步...
+    """
     t1 = time.time()
     print("1")
     # await asyncio.sleep(1)
@@ -86,6 +90,9 @@ def get_page_beautiful():
 
 
 def list_to_text(result: list):
+    """
+    列表生成器...
+    """
     a = [x.text for x in result]
     return a
 
@@ -239,7 +246,7 @@ def catch_error():
 
 def paixu():
     """
-    排序
+    排序.....
     """
     a = [1, 8, 3, 5, 3, 0, 6, 9]
     # a.sort()
@@ -286,7 +293,7 @@ def find_fun_num(l: list, n):
 
 def fib(n):
     """
-    数列
+    数列...
     """
     a, b = 0, 1
     for i in range(n):
@@ -296,7 +303,7 @@ def fib(n):
 
 def find_num_one(l: list):
     """
-    查找列表中的数字.....
+    查找列表中的单个位数的数字.....
     """
     if isinstance(l, list):
         for i in l:
@@ -306,6 +313,55 @@ def find_num_one(l: list):
     else:
         print("l is not list type")
 
+
+def find_same_number_function1(l: list):
+    """
+    方法1：
+    查找列表中的重复数字...
+    这种方法返回的结果是一个<class 'collections.Counter'>，
+    """
+    if isinstance(l, list):
+        res = Counter(l)
+        # print(res.most_common())
+        # print(res[12])
+
+    else:
+        print("l is not lsit type...")
+    pass
+
+
+def find_same_number_function2(l: list):
+    """
+    方法2：
+    这种方法直接返回的是一个字典，可以更方便的处理...
+    """
+    same_number = {}
+    if isinstance(l, list):
+        for i in l:
+            same_number[i] = l.count(i)
+
+    else:
+        print("l不是列表")
+    
+
+def find_same_number_function3(l:list):
+    """
+    方法3：
+    这种方法只能找出重复元素，还不能返回重复次数。可以结合第一个和第二个函数一起使用...
+    """
+    new_ls = []
+    same_list = []
+    same_number = {}
+    if isinstance(l, list):
+        for i in l:
+            if i not in new_ls:
+                new_ls.append(i)
+            else:
+                same_list.append(i)
+    else:
+        print("l不是列表")
+    print('重复的有：', same_list)
+    
 
 def bubble_sort(l: list):
     """
@@ -323,7 +379,7 @@ def bubble_sort(l: list):
 
 def fast_sort(l: list):
     """
-    快速排序
+    快速排序......
     """
     if isinstance(l, list):
         pass
@@ -382,20 +438,23 @@ def creatr_thing():
     
 
 def operating_redis():
-    pool = redis.ConnectionPool(host='localhost', port=6379, decode_responses=True)
-    r = redis.Redis(connection_pool=pool)
-    # print("连接成功...")
-    movie_date = r.lrange('movie_date', 0, -1)
-    movie_name = r.lrange('movie_name', 0, -1)
-    movie_url = r.lrange('movie_url', 0, -1)
-    
-    movie_date_new = list(set(movie_date))
-    movie_name_new = list(set(movie_name))
-    movie_url_new = list(set(movie_url))
+    try:
+        pool = redis.ConnectionPool(host='localhost', port=6379, decode_responses=True)
+        r = redis.Redis(connection_pool=pool)
+        # print("连接成功...")
+        movie_date = r.lrange('movie_date', 0, -1)
+        movie_name = r.lrange('movie_name', 0, -1)
+        movie_url = r.lrange('movie_url', 0, -1)
+        
+        movie_date_new = list(set(movie_date))
+        movie_name_new = list(set(movie_name))
+        movie_url_new = list(set(movie_url))
 
-    write_in_redis('movie_date_new', movie_date_new)
-    write_in_redis('movie_name_new', movie_name_new)
-    write_in_redis('movie_url_new', movie_url_new)
+        write_in_redis('movie_date_new', movie_date_new)
+        write_in_redis('movie_name_new', movie_name_new)
+        write_in_redis('movie_url_new', movie_url_new)
+    except Exception as err:
+        print(err)
     
 
 def write_in_redis(key_name: str, value: list):
@@ -421,11 +480,23 @@ def operating_mysql():
 
 
 def operating_mongodb():
-    mydb = pymongo.MongoClient('localhost', 27017)
+    try:
+        myclinent = pymongo.MongoClient('localhost', 27017)
+        mydb = myclinent.dytt_movie	# 连接到数据库
+        result_data = mydb.test	# 连接到数据库中的集合
+        # for i in result_data.find(): 
+        #     print(i)	# 查找集合中的每个数据，并输出。
+        result_one = result_data.find_one({'id': 1})
+        print(result_one)
+        print(type(result_one))  #查看单条结果的数据类型。
+    except Exception as err:
+        print(err)
     
-    pass
 
 def time_test():
+    """
+    输出时间测试...
+    """
     n = 0
     num = 0
     while n < 100000:
@@ -435,17 +506,38 @@ def time_test():
     print(num)
 
 
+def mutil_process_1():
+    """
+    多线程1...
+    """
+    for _ in range(1000):
+        print('This is test_1...')
+
+
+def mutil_process_2():
+    """
+    多线程2...
+    """
+    for _ in range(1000):
+        print('This is test_2')
+    
+
 def mutil_process():
-    print('This is test...')
-    
-    time.sleep(1)
-    
+    mutil_process_1()
+    mutil_process_2()
+    # t1 = threading.Thread(target=mutil_process_1, name='mutil_process_thread_1')
+    # t2 = threading.Thread(target=mutil_process_2, name='mutil_process_thread_2')
+    # t1.start()
+    # t2.start()
+    # t1.join()
+    # t2.join()
+
 
 def main():
     t1 = time.time()
     l = [1, 2, 12, 14, 54, 9, 0, 10, 12, 11]
-    s = '+-+++-+++---+-+-++++--'
-
+    # s = '+-+++-+++---+-+-++++--'
+    
     t2 = time.time() - t1
     print(t2)
 
