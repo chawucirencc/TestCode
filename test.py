60f2f296a3914b9d3eb2730946ce85538b91a043
unknown <1960595754@qq.com>
2019-10-14 17:02:46 +0800

    commit

--- test.py
+++ test.py
@@ -583,11 +583,59 @@ class WordFrequency():
         url = " "
         pass
 
+class Hash:
+    """
+    简单Hash表
+    """
+    def __init__(self):
+        self.capacity = 11
+        self.hash_table = [[None, None] for i in range(self.capacity)]
+        self.num = 0
+        self.load_factor = 0.75
+        
+    def hash(self, k, i):
+        # Hash函数。
+        h_value = (k+i) % self.capacity
+        if self.hash_table[h_value][0]==k:
+            return h_value
+        if self.hash_table[h_value][0]!=None:
+            i+=1
+            h_value=self.hash(k, i)
+        return h_value
+    
+    def resize(self):
+        self.capacity = self.num*2
+        temp = self.hash_table[:]
+        self.hash_table = [[None, None] for i in range(self.capacity)]
+        for i in temp:
+            # 把原来的元素存入。
+            if (i[0]!=None):
+                hash_v = self.hash(i[0], 0)
+                self.hash_table[hash_v][0] = i[0]
+                self.hash_table[hash_v][1] = i[1]
+    
+    def put(self, k, v):
+        hash_v = self.hash(k, 0)
+        self.hash_table[hash_v][0] = k
+        self.hash_table[hash_v][1] = v
+        
+        self.num += 1
+        if (self.num/len(self.hash_table))>self.load_factor:
+            self.resize()
+    
+    def get(self, k):
+        hash_v = self.hash(k, 0)
+        return self.hash_table[hash_v][1]
+                  
+
 def main():
     t1 = time.time()
     l = [1, 2, 12, 14, 54, 9, 0, 10, 12, 11]
     # s = '+-+++-+++---+-+-++++--'
-    sele()
+    hash = Hash()
+    hash.put(1, "World")
+    print(hash.get(1))
+    a = dict
     t2 = time.time() - t1
     print(t2)
 
